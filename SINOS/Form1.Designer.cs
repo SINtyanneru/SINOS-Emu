91f4e4bea5f8efe289005d96493bc09555476c86
SINtyanneru <irjitgd@gmail.com>
2022-12-09 23:35:14 +0900

    555

---
+++ SINOS/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SINOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 450);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "SINOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public Microsoft.Web.WebView2.WinForms.WebView2 webView;
+    }
+}
+
---
+++ SINOS/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Newtonsoft.Json;
+using System.Security.Authentication;
+using Microsoft.Web.WebView2.Wpf;
+using System.Runtime.InteropServices.ComTypes;
+using MediaPlayer;
+using System.Media;
+
+namespace SINOS
+{
+    public partial class Form1 : Form
+    {
+        private JsToCs CsClass = new JsToCs();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAsync();
+
+            webView.NavigationCompleted += WebView_NavigationCompleted;
+            webView.CoreWebView2InitializationCompleted += webView2_CoreWebView2InitializationCompleted;
+        }
+
+        async void InitializeAsync()
+        {
+            try
+            {
+                CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-web-security --user-data-dir=\"C://RUMISYSTEM/SINOS/OS/SYSTEM256/\"");
+                CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+                
+                await webView.EnsureCoreWebView2Async(environment);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("WebView2ランタイムがインストールされていない可能性があります。", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.Close();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+        }
+
+        private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            try
+            {
+                if (webView.CoreWebView2 != null)
+                {
+                    //JavaScriptからC#のメソッドが実行できる様に仕込む
+                    webView.CoreWebView2.AddHostObjectToScript("class", CsClass);
+                }
+                else
+                {
+                    MessageBox.Show("CoreWebView2==null");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void webView2_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            //開発者ツールを無効化
+            //webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+            //コンテキストメニュー無効化
+            webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            //ズーム無効化
+            webView.CoreWebView2.Settings.IsZoomControlEnabled = false;
+            //ステータスバー無効化
+            webView.CoreWebView2.Settings.IsStatusBarEnabled = false;
+            //キー無効化
+            //webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+
+            webView.CoreWebView2.OpenDevToolsWindow();
+
+            webView.CoreWebView2.Navigate(@"file:///C:\Users\Rumisan\source\SINOS_HTML\index.html");
+        }
+    }
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class JsToCs
+    {
+        string root = @"C:\RUMISYSTEM\SINOS\"; // ドキュメント・ルート
+
+        class Data
+        {
+            public string NAME { get; set; }
+            public string PATH { get; set; }
+            public string TYPE { get; set; }
+            public string MIME { get; set; }
+        }
+
+        public string FileListGet(string Path)
+        {
+            Console.WriteLine("[ *** ]Runing :GetFileList");
+
+            string path = root + Path.Replace("/", @"\");
+
+            //"C:\test"以下の".txt"ファイルをすべて取得する
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+            System.IO.FileInfo[] files = di.GetFiles("*", System.IO.SearchOption.TopDirectoryOnly);
+            System.IO.DirectoryInfo[] dirs = di.GetDirectories("*", System.IO.SearchOption.TopDirectoryOnly);
+
+            Console.WriteLine(files.Length);
+
+            int count = 0;
+
+            Data[] json_data = new Data[files.Length + dirs.Length];
+
+            //ディレクトリ
+            foreach (System.IO.DirectoryInfo f in dirs)
+            {
+                Console.WriteLine(f.FullName);
+
+                Console.WriteLine(f.FullName);
+                json_data[count] = new Data();
+                json_data[count].PATH = f.FullName;
+                json_data[count].NAME = f.Name;
+                json_data[count].TYPE = "DIR";
+                json_data[count].MIME = "DIR";
+                count++;
+
+            }
+            //フォルダ
+            foreach (System.IO.FileInfo f in files)
+            {
+                Console.WriteLine(f.FullName);
+
+                Console.WriteLine(f.FullName);
+                json_data[count] = new Data();
+                json_data[count].PATH = f.FullName;
+                json_data[count].NAME = f.Name;
+                json_data[count].TYPE = "FILE";
+                json_data[count].MIME = f.Name.Split('.')[f.Name.Split('.').Length - 1];
+                Console.WriteLine(f.Name.Split('.')[f.Name.Split('.').Length - 1]);
+                count++;
+            }
+
+            string jsonStr = JsonConvert.SerializeObject(json_data);
+
+            return jsonStr;
+        }
+
+        public string FileDataGet(string Path)
+        {
+            try
+            {
+                string path = root + Path.Replace("/", @"\");
+
+                switch (Path.Split('.')[Path.Split('.').Length - 1])
+                {
+                    case "png":
+                    case "jpg":
+                    case "jpeg":
+                    case "gif":
+                    case "bmp":
+                        Bitmap bitmap = new Bitmap(path);
+
+                        return BitmapToBase64String(bitmap);
+
+                    default:
+                        return File.ReadAllText(path);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return null;
+        }
+
+        public string FileExists(string Path)
+        {
+            string path = root + Path.Replace("/", @"\");
+            if (System.IO.File.Exists(path))
+            {
+                //ファイルが存在します
+                return "EXT";
+            }
+            else
+            {
+                //ファイルが存在しません
+                return "NEXT";
+            }
+        }
+
+        public string PlaySound(string Path)
+        {
+            try
+            {
+                string path = root + Path.Replace("/", @"\");
+                SoundPlayer player= new SoundPlayer(path);
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("PlaySound:" + ex.Message);
+            }
+
+            return null;
+        }
+
+        public string FileTextGet(string Path)
+        {
+            try
+            {
+                string path = root + Path.Replace("/", @"\");
+
+                return File.ReadAllText(path);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return null;
+        }
+
+        public void SaveFile(string Path, string DATA)
+        {
+            try
+            {
+                string path = root + Path.Replace("/", @"\");
+
+                File.WriteAllText(path, DATA);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public string OpenFileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            return ofd.FileName.Replace(@"C:\RUMISYSTEM\SINOS","").Replace(@"\","/");
+        }
+
+
+        string BitmapToBase64String(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] bites = ms.ToArray();
+                return Convert.ToBase64String(bites);
+            }
+        }
+    }
+}
